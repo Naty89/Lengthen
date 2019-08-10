b0834377f80a0872168b34023748921f7507beac
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-10 13:55:04 -0400

    Update lengthen.py

--- lengthen.py
+++ lengthen.py
@@ -1,27 +1,17 @@
 def lengthen(stri, x):
-# x = "stockjobbingsis"
+x = "stockjobbingsis"
 
-# str = "forest"
+str = "forest"
 
-# if len(str) < len(x):
-# 	u = len(x)/len(str)
-# 	e = str*u
+if len(str) < len(x):
+	u = len(x)/len(str)
+	e = str*u
 
-# 	print e + str[0:len(x)-len(e)]
-# if len(str) > len(x):
-# 	u =  len(str)/len(x)
-# 	e = x * u
-# 	print e + x[0:len(str)-len(e)]
-
-	if len(stri) > len(x):
-		longer = stri
-		shorter = x
-	else:
-		longer = x
-		shorter = stri
-
-	return shorter * (len(longer)//len(shorter)) + shorter[0:len(longer)%len(shorter)]
-
+	print e + str[0:len(x)-len(e)]
+if len(str) > len(x):
+	u =  len(str)/len(x)
+	e = x * u
+	print e + x[0:len(str)-len(e)]
 
 print lengthen("ingenius", "forest")
 print lengthen("clap", "skipping")
